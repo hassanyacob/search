797692ea92c30950fd0f6611cdd0183fafbd57d8
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2021-03-01 21:01:15 +0000

    Update live search.js

--- live search.js
+++ live search.js
@@ -7,9 +7,9 @@ const states = await res.json();
 
 console.log(states);
 // get matches to current text input 
-let matches = states.filter(states => {
-   const regex = new RegExp('^${searchText}', 'gi'); 
-   return states.name.match(regex) || state.abbr.match(regex);
+let matches = states.filter(state => {
+   const regex = new RegExp(`^${searchText}`, 'gi'); 
+   return state.name.match(regex) || state.abbr.match(regex);
  });
     if (searchText.length === 0) {
         matches= [];
