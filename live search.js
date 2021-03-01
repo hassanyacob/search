87a3358278a098f95db7ec702ccef20dbfb79b0a
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2021-03-01 19:18:21 +0000

    Update live search.js

--- live search.js
+++ live search.js
@@ -1,10 +1,16 @@
 const search = document.getElementById('search');
 const matchlist = document.getElementById('match-list');
 //search states.json and filter it
-const searchStates = async searchText =>{
+const searchStates = async searchText => {
 const res = await fetch(state_capitals.json);   
 const states = await res.json():
 
 console.log(states);
-}
+// get matches to current text input 
+//let matches = states.filter(states => {
+// const regex = new RegExp('^${searchText}', 'gi'); 
+//    return states.name.match(regex) || state.abbr.match(regex);
+// });
+ console.log(matches);   
+};
 search.addEventListener('input',() => searchStates(search.value));
