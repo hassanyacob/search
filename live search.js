5cedcee15eecb25460dda36a837722d64671d6cc
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2021-03-01 20:19:57 +0000

    Update live search.js

--- live search.js
+++ live search.js
@@ -2,15 +2,31 @@ const search = document.getElementById('search');
 const matchlist = document.getElementById('match-list');
 //search states.json and filter it
 const searchStates = async searchText => {
-const res = await fetch('states.json');   
-const states = await res.json();
+const res = await fetch(states.json);   
+const states = await res.json():
 
 console.log(states);
 // get matches to current text input 
-//let matches = states.filter(states => {
-// const regex = new RegExp('^${searchText}', 'gi'); 
-//    return states.name.match(regex) || state.abbr.match(regex);
-// });
-// console.log(matches);   
+let matches = states.filter(states => {
+   const regex = new RegExp('^${searchText}', 'gi'); 
+   return states.name.match(regex) || state.abbr.match(regex);
+ });
+    if (searchText.length ===0){
+        matches= [];
+        matchlist.innerHTML = '';
+    }
+ outputhtml(matches);   
 };
+// show results in html 
+const outputhtml = matches => {
+    if(matches.length > 0){
+        const html =matches.map(match => '
+        <div class="card card-body mb-1">
+         <h4>${match.name} (${match.abbr})<span class="text-primary">${match.capital}</span></h4> 
+        <small>lat: ${match.lat} / long: ${match.long}</small>
+        </div>
+        ').join('');
+        matchlist.innerHTML = html;
+    }
+}
 search.addEventListener('input',() => searchStates(search.value));
