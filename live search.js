8fc45116aa74afdeef43cee8b40d51375133126a
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2021-03-01 19:27:50 +0000

    Update live search.js

--- live search.js
+++ live search.js
@@ -2,7 +2,7 @@ const search = document.getElementById('search');
 const matchlist = document.getElementById('match-list');
 //search states.json and filter it
 const searchStates = async searchText => {
-const res = await fetch(state_capitals.json);   
+const res = await fetch(../data/states.json);   
 const states = await res.json();
 
 console.log(states);
